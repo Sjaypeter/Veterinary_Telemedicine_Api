1a8b70b395a41164637627aec43f2c305b60834a
Sjaypeter <Sjaypeter.sjp@gmail.com>
2025-10-02 10:41:54 +0100

    Viewsets added

--- Vetcare/Telemedicine/views.py
+++ Vetcare/Telemedicine/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import OwnerProfile, Vetprofile, PetProfile, Appointment, MedicalRecord
+from .serializers import OwnerProfileSerializer, VetprofileSerializer, PetProfileSerializer, AppointmentSerializer, MedicalRecordSerializer
 # Create your views here.
+
+class OwnerProfileView(viewsets.ModelViewSet):
+    queryset = OwnerProfile.objects.all()
+    serializer_class = OwnerProfileSerializer
+    permission_classes = [IsAuthenticated]
+    
+
+class VetprofileViewset(viewsets.ModelViewSet):
+    queryset = Vetprofile.objects.all()
+    serializer_class = VetprofileSerializer
+    permission_classes = [IsAuthenticated]
+    
+
+class PetProfileViewset(viewsets.ModelViewSet):
+    queryset = PetProfile.objects.all()
+    serializer_class = VetprofileSerializer
+    permission_classes = [IsAuthenticated]
+
+    
+class AppointmentViewset(viewsets.ModelViewSet):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class MedicalRecordView(viewsets.ModelViewSet):
+    queryset = MedicalRecord.objects.all()
+    serializer_class= MedicalRecordSerializer
+    permission_classes = [IsAuthenticated]
+    
+    
+    
