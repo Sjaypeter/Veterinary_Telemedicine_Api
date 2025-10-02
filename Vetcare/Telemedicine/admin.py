c60b6624d36ee7cd206a79038d5175b9950bdabf
Sjaypeter <Sjaypeter.sjp@gmail.com>
2025-10-02 10:00:54 +0100

    Owner profile, medical record, appointment, petprofile models

--- Vetcare/Telemedicine/admin.py
+++ Vetcare/Telemedicine/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Vetprofile,Appointment,MedicalRecords,PetProfile,OwnerProfile
 # Register your models here.
+admin.site.register(Vetprofile)
+admin.site.register(Appointment)
+admin.site.register(MedicalRecords)
+admin.site.register(PetProfile)
+admin.site.register(OwnerProfile)
