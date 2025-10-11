87b960d5bc341eb25111552a8d53a8f43799a757
Sjayp <Sjaypeter.sjp@gmail.com>
2025-10-11 15:03:02 +0100

    Removed telemedicine app

--- Vetcare/Telemedicine/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . models import Appointment,MedicalRecord,PetProfile,OwnerProfile, Vetprofile
-# Register your models here.
-
-admin.site.register(Appointment)
-admin.site.register(MedicalRecord)
-admin.site.register(PetProfile)
-admin.site.register(OwnerProfile)
-admin.site.register(Vetprofile)
