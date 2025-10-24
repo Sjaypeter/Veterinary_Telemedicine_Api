977a4ec704fc85ed3b902d10b9373214b233bb29
Sjayp <Sjaypeter.sjp@gmail.com>
2025-10-24 02:14:13 +0100

    settings for CORS

--- Vetcare/settings.py
+++ Vetcare/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -183,3 +184,14 @@ SIMPLE_JWT = {
 # CORS CONFIG (for testing frontend or Postman)
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
+
+# CSRF settings for API
+CSRF_TRUSTED_ORIGINS = [
+    "https://veterinarytelemedicineapi-production.up.railway.app",
+]
+
+CSRF_COOKIE_SECURE = False
+CSRF_COOKIE_HTTPONLY = False
+SESSION_COOKIE_SECURE = False
